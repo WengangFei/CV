30bbf3f5ac4d417ec9b85d5d2a4054239e19333f
wengang fei <78629358+jujubeefei@users.noreply.github.com>
2022-08-10 10:54:35 -0400

    Update js_file.js

--- js_file.js
+++ js_file.js
@@ -1,6 +1,36 @@
-var mount = document.getElementsByClassName('mount');
-var sum = 0;
-for(let item of mount){
-    sum += parseInt(item.textContent.substring(1));
+//Set a reference to the animated element
+const animated = document.getElementById("name");
+const job = document.getElementById("job");
+
+//Add animation event listener, with attached function.
+animated.addEventListener('animationend', () => {
+    job.style.visibility = 'visible';
+    job.style.transform = 'rotate(-360deg)';
+    job.style.color = '#1c6864';
+});
+
+const signal = document.getElementsByClassName("circle");
+signal[0].addEventListener('animationend',()=>{
+    signal[0].style.transform = 'rotate(180deg)';
+});
+
+const slogan = document.getElementsByClassName('second_part_title_slogan');
+const pic = document.getElementsByClassName('second_part_img_pic');
+for(let i=0;i<slogan.length;i++){
+    pic[i].addEventListener('mouseover',()=>{
+        //console.log('hi');
+        slogan[i].style.visibility = 'visible';
+        slogan[i].style.transform = 'scale(1.2)';
+        slogan[i].style.transition = 'all 3s';
+
+    });
 }
-document.getElementsByClassName('sum')[0].textContent = '$' + sum;
+
+const contact = document.getElementsByClassName('contact');
+const contactMe = document.getElementsByClassName('corner');
+contactMe[0].addEventListener('mouseover',()=>{
+    contact[0].style.visibility = 'visible';
+    contact[0].style.color = 'black';
+    contact[0].style.transform = 'scale(1.2)';
+    contact[0].style.transition = 'all 2s';
+});
